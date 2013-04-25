e2c5957c1d162ac59cc59ac26287dfd148c57d78
zasmail <zasmail@utexas.edu>
2013-04-25 02:16:15 -0500

    added queues

--- src/tasker.cpp
+++ src/tasker.cpp
@@ -2,17 +2,27 @@
 #include "std_msgs/String.h"
 #include "std_msgs/Header.h"
 #include "taskallocator/Request.h"
+#include <math.h>
 #include <queue>
 
+queue<float> tasks;
+float totalTime;
 
+
+float getDistance (float startX, float startY, float endX, float endY)
+{
+  return sqrt(pow(endX-startX,2)+pow(endY-startY,2));
+
+
+}
 	
-
 void processRequest(taskallocator::Request msg){
 	ROS_INFO("Message type: %s",msg.taskType.c_str());
 	ROS_INFO("Request device %s", msg.requestDevice.c_str());
 	ROS_INFO("Start - (%f,%f) End - (%f,%f)",msg.startX, msg.startY, msg.endX, msg.endY);
+	float distance =getDistance (msg.startX, msg.startY, msg.endX, msg.endY);
+	totalTime+=distance;
 
-	
 
 }
 
