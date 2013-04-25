267040463e1a075a0cd66200772cd1ba7f46a58c
Greg Cerna <gregcerna@gmail.com>
2013-04-25 02:38:11 -0500

    update to tasker

--- src/tasker.cpp
+++ src/tasker.cpp
@@ -4,34 +4,36 @@
 #include "taskallocator/Request.h"
 #include <math.h>
 #include <queue>
+#include <iostream>
 
-queue<float> tasks;
+
+std::queue<float> tasks;
 float totalTime;
 
 
-float getDistance (float startX, float startY, float endX, float endY)
-{
+float getDistance (float startX, float startY, float endX, float endY){
   return sqrt(pow(endX-startX,2)+pow(endY-startY,2));
-
-
 }
 	
 void processRequest(taskallocator::Request msg){
-	ROS_INFO("Message type: %s",msg.taskType.c_str());
-	ROS_INFO("Request device %s", msg.requestDevice.c_str());
-	ROS_INFO("Start - (%f,%f) End - (%f,%f)",msg.startX, msg.startY, msg.endX, msg.endY);
+	
 	float distance =getDistance (msg.startX, msg.startY, msg.endX, msg.endY);
+
+	tasks.push(distance);
 	totalTime+=distance;
 
-
+	ROS_INFO("Message type: %s",msg.taskType.c_str());
+	ROS_INFO("Request device %s", msg.requestDevice.c_str());
+	ROS_INFO("Distance - %f ",distance);
+	ROS_INFO("Total Time - %f",totalTime);
+	ROS_INFO("Total Tasks - %d",(int)tasks.size());
 }
 
 int main (int argc, char **argv) {
+
 	ros::init(argc, argv, "tasker");
 	ros::NodeHandle n;
-	unsigned int buffer = 1000;
-
-	ros::Subscriber sub = n.subscribe<taskallocator::Request>("requests", buffer, processRequest);
+	ros::Subscriber sub = n.subscribe<taskallocator::Request>("requests", 1000, processRequest);
 	ros::spin();
 	return 0;
 
